fe4d1e72c722b4ac23eddeb5f0f5eab6f7c38dde
Paul-Maple <130990249+Paul-Maple@users.noreply.github.com>
2025-09-28 14:47:14 +0300

    Шаманю с кодировками

--- source/test.c
+++
@@ -1,7 +0,0 @@
-#include "test.h"
-#include "timer.h"
-
-void test_init(void)
-{
-    
-}
--- source/test.h
+++
@@ -1,7 +0,0 @@
-#ifndef __TEST_H
-#define __TEST_H
-
-// Инициализация модуля для тестов
-void test_init(void);
-
-#endif // __TEST_H
