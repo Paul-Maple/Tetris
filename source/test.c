757eabb1a840887e4566eced49e4135e6662c40d
Павел <130990249+Paul-Maple@users.noreply.github.com>
2025-10-03 17:13:55 +0300

    тест пустой

--- source/test.c
+++ source/test.c
@@ -1,16 +1,7 @@
 #include "test.h"
-#include "lcd.h"
+
 
 void test_init(void)
 {
-    // Красный пиксель посередине экрана
-    lcd_position_t pos = 
-    {
-        .x[0] = 120,
-        .x[1] = 121,
-        .y[0] = 160,
-        .y[1] = 161
-    };
     
-    lcd_image_set(&pos, LCD_COLOR_RED);
 }
