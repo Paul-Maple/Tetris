f8596aceb99eb5367525368201c766e4335431a9
Павел <130990249+Paul-Maple@users.noreply.github.com>
2025-10-06 12:13:09 +0300

    перенёс в тест вызов отрисовки пикселя

--- source/test.c
+++ source/test.c
@@ -1,7 +1,18 @@
 #include "test.h"
-
+#include <lcd.h>
 
 void test_init(void)
 {
+    /*
+    // Красный пиксель посередине экрана
+    lcd_position_t position = 
+    {
+        .x[0] = 120,
+        .x[1] = 121,
+        .y[0] = 160,
+        .y[1] = 161
+    };
     
+    lcd_image_set(&position, LCD_COLOR_RED); 
+    */
 }
