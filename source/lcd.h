6d5e59715dbb79483bda16000fa82bbbfe2f3cc5
Павел <130990249+Paul-Maple@users.noreply.github.com>
2025-10-08 09:58:45 +0300

    переместил некоторые элементы из .h в .c

--- source/lcd.h
+++ source/lcd.h
@@ -1,49 +1,7 @@
 #ifndef __LCD_H
 #define __LCD_H
 
-//#include <typedefs.h>
-#include <timer.h>
-
-// Перечисление команд управления дисплеем
-enum
-{    
-    LCD_CMD_SOFT_RESET      = 0x01,     // Программный сброс
-    LCD_CMD_SLEEP_OUT       = 0x11,     // Выход из режима сна       
-    LCD_CMD_NORMAL_MODE     = 0x13,     // Нормальный режим отображения
-    LCD_CMD_DISPLAY_ON      = 0x29,     // Включение дисплея
-    LCD_CMD_DISPLAY_OFF     = 0x28,     // Выключение дисплея
-    LCD_CMD_MAC_SET         = 0x36,     // Настройка доступа к памяти
-    LCD_CMD_COLLUM_SET      = 0x2A,     // Установка адреса столбца
-    LCD_CMD_LINE_SET        = 0x2B,     // Установка адреса строки    
-    LCD_CMD_MEMORY_SET      = 0x2C,     // Передача данных от МК до кадровой памяти
-    LCD_PIXEL_FORMAT_SET    = 0x3A,     // Установка формата RGB
-    LCD_GAMMA_SET           = 0x26,     // Установка гаммы
-    LCD_CMD_COLOR_SET       = 0x2D,     // Преобразование цветовой глубины с 16 бит в 18 бит
-    
-    LCD_CMD_NOP             = 0x00      // Пустая команда (Для завершения передачи)
-    // TODO: Добавить необходимые команды, лишние убрать
-};
-
-// Перечисление цветов (Формат: 16 бит)
-enum
-{
-    LCD_COLOR_RED = 0xF800, 
-    // TODO: Дописать цвета
-};
-
-// Перечисление тиков таймера для задержки отправки команд
-enum
-{
-    LCD_TIME_DELAY_5MS   = TIMER_TICKS_MS(5),
-    LCD_TIME_DELAY_120MS = TIMER_TICKS_MS(120)
-};
-
-// Перечисление состояний пина DCRS 
-enum
-{
-    LCD_CMD  = 0,    // Команда
-    LCD_DATA = 1     // Данные
-};
+#include <typedefs.h>
 
 // Структура элемента цепочки команд
 typedef struct
@@ -56,6 +14,13 @@ typedef struct
     uint8_t cmd;
     
 } lcd_chain_cmd_t;
+
+// Перечисление цветов (Формат: 16 бит)
+enum
+{
+    LCD_COLOR_RED = 0xF800, 
+    // TODO: Дописать цвета
+};
 
 // Структура координат записи
 typedef struct
