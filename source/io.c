62ae16b264e20ce94a498df6ade938e9bfeaebb5
Павел <130990249+Paul-Maple@users.noreply.github.com>
2025-10-10 12:10:32 +0300

    Пин CSX теперь настроен на подтяжку вверх

--- source/io.c
+++ source/io.c
@@ -13,7 +13,7 @@ void io_init(void)
         IO_AF_PD(IO_LCD_SCL_PIN, 5);                                            // Пин тактирования SPI
         IO_OUT_PD(IO_LCD_LED_PIN);                                              // Пин подсветки дисплея
         IO_OUT_PD(IO_LCD_DCRS_PIN);                                             // Пин выбора команды/данных
-        IO_AF_PD(IO_LCD_CSX_PIN, 5);                                            // Пин выбора slave-устройства
+        IO_AF_PU(IO_LCD_CSX_PIN, 5);                                            // Пин выбора slave-устройства
         IO_NC(5);
         IO_NC(6);
         IO_AF_PD(IO_LCD_SDA_PIN, 5);                                            // Пин для передачи данных
