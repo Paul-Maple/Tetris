a61879d85886aa7e82de1ae90e6a77f2e428f343
Павел <130990249+Paul-Maple@users.noreply.github.com>
2025-10-17 17:02:22 +0300

    Убраны макросы для настройки SYSCFG

--- source/button.c
+++ source/button.c
@@ -1,12 +1,6 @@
 #include "button.h"
 #include <nvic.h>
 
-// Настройка регистра SYSCFG для выбора порта внешних прерываний
-#define BUTTON_EXTI0_PORTC      (SYSCFG_EXTICR1_EXTI0 & SYSCFG_EXTICR1_EXTI0_PC)
-#define BUTTON_EXTI1_PORTC      (SYSCFG_EXTICR1_EXTI1 & SYSCFG_EXTICR1_EXTI1_PC)
-#define BUTTON_EXTI2_PORTC      (SYSCFG_EXTICR1_EXTI2 & SYSCFG_EXTICR1_EXTI2_PC)
-#define BUTTON_EXTI3_PORTC      (SYSCFG_EXTICR1_EXTI3 & SYSCFG_EXTICR1_EXTI3_PC)
-
 // Сброс флага прерывания (При записи "1")
 #define BUTTON_ISR_FLAG_CLEAR(line)     (EXTI->PR1 |= EXTI_PR1_PIF##line)
 
@@ -14,17 +8,12 @@
 // TODO: Подобрать правильное время выжидания
 #define BUTTON_CONTACT_BOUNCE_TIME      TIMER_TICKS_MS(10)
 
-// Статическая инициализация кнопки
-#define BUTTON_STATIC_INIT(mode, timer_cb, pressed_event_cb)                    \
+// Статическая инициализация кнопки         
+#define BUTTON_STATIC_INIT(mode, timer_cb)                                      \
 {                                                                               \
     TIMER_STATIC_INIT(mode, timer_cb),                                          \
-    .pressed = false,                                                           \
-    EVENT_STATIC_INIT(pressed_event_cb)                                         \
-    /* EVENT_STATIC_INIT(released_event_cb) */                                  \
+    .pressed = false                                                            \
 }
-
-// Функция для теста кнопок (уже не работает, функция пустая)
-extern void test_func(void);
 
 // Обработчик события нажатия кнопки
 /* Вызываются как callback у таймера в случае, *
@@ -42,8 +31,8 @@ static void button_pressed_event_cb(timer_t *timer)
         // Установка флага нажатия кнопки
         button->pressed = true; 
         
-        // Вызов события по нажатию кнопки
-        event_raise(&button->pressed_event);
+        // Оповещение модулей о нажатии кнопки
+        // 
         return;
     }
     
@@ -53,13 +42,13 @@ static void button_pressed_event_cb(timer_t *timer)
         // Установка флага: кнопка не нажата
         button->pressed = false;
         
-        // Вызов события по отпусканию кнопки
-        //event_raise(&button->released_event);
+        // Оповещение модулей об отпускании кнопки
+        // 
     }
 }
 
 // Кнопки
-button_t button_0 = BUTTON_STATIC_INIT(TIMER_MODE_ONE_SHOT, button_pressed_event_cb, test_func);
+static button_t button_0 = BUTTON_STATIC_INIT(TIMER_MODE_ONE_SHOT, button_pressed_event_cb);
 //button_t button_1 = BUTTON_STATIC_INIT(TIMER_MODE_ONE_SHOT, button_pressed_event_cb);
 //button_t button_2 = BUTTON_STATIC_INIT(TIMER_MODE_ONE_SHOT, button_pressed_event_cb);
 //button_t button_3 = BUTTON_STATIC_INIT(TIMER_MODE_ONE_SHOT, button_pressed_event_cb);
@@ -76,7 +65,10 @@ void button_init(void)
     EXTI->FTSR1 |= EXTI_FTSR1_FT0 | EXTI_FTSR1_FT1 | EXTI_FTSR1_FT2 | EXTI_FTSR1_FT3;
     
     // Прерывания на пинах 0, 1, 2, 3 порта С
-    SYSCFG->EXTICR[0] = BUTTON_EXTI0_PORTC | BUTTON_EXTI1_PORTC | BUTTON_EXTI2_PORTC| BUTTON_EXTI3_PORTC;
+    SYSCFG->EXTICR[0] = (SYSCFG_EXTICR1_EXTI0 & SYSCFG_EXTICR1_EXTI0_PC) | 
+                        (SYSCFG_EXTICR1_EXTI1 & SYSCFG_EXTICR1_EXTI1_PC) | 
+                        (SYSCFG_EXTICR1_EXTI2 & SYSCFG_EXTICR1_EXTI2_PC) | 
+                        (SYSCFG_EXTICR1_EXTI3 & SYSCFG_EXTICR1_EXTI3_PC);
     
     // Включить перывания в NVIC
     nvic_irq_enable(EXTI0_IRQn);
@@ -97,15 +89,27 @@ void button_0_iqr(void)
 
 void button_1_iqr(void)
 {
+    // Сбросить флаг прерывания
     BUTTON_ISR_FLAG_CLEAR(1);
+        
+    // Запуск таймера для выжидания дребезга контактов
+    timer_start(&button_0.timer, BUTTON_CONTACT_BOUNCE_TIME);
 }
 
 void button_2_iqr(void)
 {
+    // Сбросить флаг прерывания
     BUTTON_ISR_FLAG_CLEAR(2);
+        
+    // Запуск таймера для выжидания дребезга контактов
+    timer_start(&button_0.timer, BUTTON_CONTACT_BOUNCE_TIME);
 }
 
 void button_3_iqr(void)
 {
+    // Сбросить флаг прерывания
     BUTTON_ISR_FLAG_CLEAR(3);
+        
+    // Запуск таймера для выжидания дребезга контактов
+    timer_start(&button_0.timer, BUTTON_CONTACT_BOUNCE_TIME);
 }
