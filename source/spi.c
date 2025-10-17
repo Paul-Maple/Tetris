7618fc963db995197be19d127b6ce02953c7d593
Павел <130990249+Paul-Maple@users.noreply.github.com>
2025-10-17 17:05:38 +0300

    Переделана логика функции spi_transmit()

--- source/spi.c
+++ source/spi.c
@@ -1,6 +1,7 @@
 #include "spi.h"
 #include <mcu.h>
 #include <nvic.h>
+#include "io.h"
 
 // Подготовка SPI к передаче
 static void spi_preparing(void)
@@ -10,9 +11,10 @@ static void spi_preparing(void)
     
     /* Регистры CR1 и CR2 должны быть настроены перед включением SPI */
     // Настройка регистров SPI
-    SPI1->CR1 = SPI_CR1_BR_0 | SPI_CR1_BR_1 | SPI_CR1_BR_2 |    // Делитель частоты на 256
+    SPI1->CR1 = SPI_CR1_BR_1 | SPI_CR1_BR_2 |                        // Делитель частоты на 32
                 SPI_CR1_MSTR |                                     // Режим мастера
-                SPI_CR1_BIDIMODE | SPI_CR1_BIDIOE;                 // Однонапрвленный режим, Tx-only
+                SPI_CR1_BIDIMODE | SPI_CR1_BIDIOE;// |                // Однонапрвленная передача по 1 проводу + Tx-only
+                //SPI_CR1_SSM | SPI_CR1_SSI;                         // Программное управление NSS + NSS state "1"
     
     SPI1->CR2 = SPI_CR2_DS_0 | SPI_CR2_DS_1 | SPI_CR2_DS_2 |    // Передача по 8 бит
                 SPI_CR2_SSOE |                                  // Вывод NSS включен и управляется SPI аппаратно
@@ -35,16 +37,32 @@ void spi_disable(void)
     RCC->APB2ENR &= ~RCC_APB2ENR_SPI1EN;
 }
 
-void spi_transmit(const uint8_t *data)
+void spi_transmit(const uint8_t *data, const uint8_t size)
 {
     ASSERT_NULL_PTR(data);
     
-    // Ожидание освобождения буфера передатчика
-    while (!(SPI1->SR & SPI_SR_TXE));
+    // Включить SPI
+    spi_enable();
     
-    // Запись в регистр данных
-    SPI1->DR = (*data);
+    // NSS low
+    //io_nss_set(0);
+    
+    // Запись данных в буфер для отправки ( Big-Endian + MSB in SPI )
+    for (uint8_t i = 0; i < size; i++)
+    {
+        // Ожидание освобождения буфера передатчика
+        while (!(SPI1->SR & SPI_SR_TXE));
+        
+        // Запись в регистр данных
+        SPI1->DR = *(data + i);
+    }
     
     // Ожидание окончания передачи
     while (SPI1->SR & SPI_SR_BSY);
+    
+    // NSS high
+    //io_nss_set(1);
+    
+    // Отключить SPI
+    spi_disable();
 }
