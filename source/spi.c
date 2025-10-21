d120988fd4e31f5a91fecfb593f6d0916bf81044
Павел <130990249+Paul-Maple@users.noreply.github.com>
2025-10-21 12:12:15 +0300

    В настройки SPI добавлена фаза сигнала
    +Переделана логика функции отправки данных

--- source/spi.c
+++ source/spi.c
@@ -11,12 +11,13 @@ static void spi_preparing(void)
     
     /* Регистры CR1 и CR2 должны быть настроены перед включением SPI */
     // Настройка регистров SPI
-    SPI1->CR1 = SPI_CR1_BR_1 | SPI_CR1_BR_2 |                        // Делитель частоты на 32
-                SPI_CR1_MSTR |                                     // Режим мастера
-                SPI_CR1_BIDIMODE | SPI_CR1_BIDIOE;// |                // Однонапрвленная передача по 1 проводу + Tx-only
+    SPI1->CR1 = SPI_CR1_BR_0 | SPI_CR1_BR_1 | SPI_CR1_BR_2 |           // Делитель частоты на 256
+                SPI_CR1_MSTR |                                         // Режим мастера
+                SPI_CR1_CPHA;//                                         // Фаза сигнала
+                //SPI_CR1_BIDIMODE | SPI_CR1_BIDIOE;// |                // Однонапрвленная передача по 1 проводу + Tx-only
                 //SPI_CR1_SSM | SPI_CR1_SSI;                         // Программное управление NSS + NSS state "1"
     
-    SPI1->CR2 = SPI_CR2_DS_0 | SPI_CR2_DS_1 | SPI_CR2_DS_2 |    // Передача по 8 бит
+    SPI1->CR2 = SPI_CR2_DS_0 | SPI_CR2_DS_1 | SPI_CR2_DS_2 | SPI_CR2_DS_3 |   // Передача по 16 бит
                 SPI_CR2_SSOE |                                  // Вывод NSS включен и управляется SPI аппаратно
                 SPI_CR2_NSSP;                                   // Вывод NSS "1" между передачами
 }
@@ -37,31 +38,27 @@ void spi_disable(void)
     RCC->APB2ENR &= ~RCC_APB2ENR_SPI1EN;
 }
 
-void spi_transmit(const uint8_t *data, const uint8_t size)
+void spi_transmit(const uint16_t *data)
 {
     ASSERT_NULL_PTR(data);
     
     // Включить SPI
     spi_enable();
     
-    // NSS low
-    io_nss_set(0);
+    // Ожидание освобождения буфера передатчика
+    while (!(SPI1->SR & SPI_SR_TXE));
     
-    // Запись данных в буфер для отправки ( Big-Endian + MSB in SPI )
-    for (uint8_t i = 0; i < size; i++)
-    {
-        // Ожидание освобождения буфера передатчика
-        while (!(SPI1->SR & SPI_SR_TXE));
-        
-        // Запись в регистр данных
-        SPI1->DR = *(data + i);
-    }
+    // Запись в регистр данных
+    SPI1->DR = *(data);
+    
+    // NSS low
+    io_nss_set(false);
     
     // Ожидание окончания передачи
     while (SPI1->SR & SPI_SR_BSY);
     
     // NSS high
-    io_nss_set(1);
+    io_nss_set(true);
     
     // Отключить SPI
     spi_disable();
