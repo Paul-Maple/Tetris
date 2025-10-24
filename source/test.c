7ce02ad4f6f6246d0d1625b4625602c188aafa99
Павел <130990249+Paul-Maple@users.noreply.github.com>
2025-10-24 15:19:46 +0300

    Закраска большой области красным цветом

--- source/test.c
+++ source/test.c
@@ -6,14 +6,14 @@ void test_init(void)
     // Установка координат
     const lcd_position_t position = 
     {
-        .x1 = 120,
-        .x2 = 121,
-        .y1 = 160,
-        .y2 = 161
+        .x1 = 40,
+        .x2 = 200,
+        .y1 = 40,
+        .y2 = 300
     };
     
     // Красный пиксель посередине экрана
-    lcd_image_set(&position, LCD_COLOR_GREEN); 
+    lcd_image_set(&position, LCD_COLOR_RED); 
 }
 
 void test_func(void)
