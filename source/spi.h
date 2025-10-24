0ddd0c96554029a2b412c5ed16a4b2946593e15c
Павел <130990249+Paul-Maple@users.noreply.github.com>
2025-10-24 15:45:04 +0300

    Добавил функцию для отправки цвета

--- source/spi.h
+++ source/spi.h
@@ -6,4 +6,7 @@
 // Передача 8 бит данных
 void spi_transmit(const uint8_t data);
 
+// Передача 16-битного цвета
+void spi_transmit_color(const uint16_t color, const uint32_t size);
+
 #endif // __SPI_H
