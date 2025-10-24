85039a87887dc46119f646f86e6e5ca4af2c3f75
Павел <130990249+Paul-Maple@users.noreply.github.com>
2025-10-24 15:45:41 +0300

    Итоговая конфигурация SPI для успешной инициализации дисплея

--- source/spi.c
+++ source/spi.c
@@ -2,32 +2,36 @@
 #include <nvic.h>
 #include "io.h"
 
+// Макросы для настрйки регистра DS для передачи 8 или 16 бит
+#define SPI_DS_8_BIT        (SPI_CR2_DS_0 | SPI_CR2_DS_1 | SPI_CR2_DS_2)
+#define SPI_DS_16_BIT       (SPI_CR2_DS_0 | SPI_CR2_DS_1 | SPI_CR2_DS_2 | SPI_CR2_DS_3)
+
 // Подготовка SPI к передаче
-static void spi_preparing()
+static void spi_preparing(uint32_t SPI_DS)
 {
     // Включить тактирование SPI1
     RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
     
     /* Регистры CR1 и CR2 должны быть настроены перед включением SPI */
     // Настройка регистров SPI
-    SPI1->CR1 = SPI_CR1_BR_1 | SPI_CR1_BR_2 |                                   // Делитель частоты на 128
-                SPI_CR1_MSTR |                                                  // Режим мастера
-                SPI_CR1_BIDIMODE | SPI_CR1_BIDIOE;// |                              // Simplex mode, Tx-Only
-                //SPI_CR1_CPHA;                                                 // Фаза сигнала
+    SPI1->CR1 = SPI_CR1_MSTR |                                                  // Режим мастера
+                SPI_CR1_BIDIMODE | SPI_CR1_BIDIOE;                              // Simplex mode, Tx-Only
     
-    SPI1->CR2 = SPI_CR2_DS_0 | SPI_CR2_DS_1 | SPI_CR2_DS_2 |                    // Передача по 8 бит
-                SPI_CR2_SSOE; |                                                  // Вывод NSS включен и управляется SPI аппаратно
+    SPI1->CR2 = SPI_DS |                                                        // Передача по 8 бит
+                SPI_CR2_SSOE;                                                   // Вывод NSS включен и управляется SPI аппаратно
 }
 
-static void spi_enable(void)
+// Включение SPI
+static void spi_enable(uint32_t SPI_DS)
 {
     // Подготовка тактирования и настройка шины SPI
-    spi_preparing();
+    spi_preparing(SPI_DS);
     
     // Включить SPI
     SPI1->CR1 |= SPI_CR1_SPE; 
 }
 
+// Отключение SPI
 static void spi_disable(void)
 {
     // Отключение шины SPI и тактирования SPI
@@ -38,7 +42,7 @@ static void spi_disable(void)
 void spi_transmit(const uint8_t data)
 {
     // Включить SPI
-    spi_enable();
+    spi_enable(SPI_DS_8_BIT);
     
     // Ожидание освобождения буфера передатчика
     while (!(SPI1->SR & SPI_SR_TXE));
@@ -48,16 +52,32 @@ void spi_transmit(const uint8_t data)
     *(( __IO uint8_t * ) &SPI1->DR) = data;
     // SPI1->DR = data;
     
-    // NSS low
-    io_nss_set(false);
-    
     // Ожидание освобождения TxFIFO
-    while (!(SPI_SR_FTLVL_0 | SPI_SR_FTLVL_1));
+    //while (!(SPI_SR_FTLVL_0 | SPI_SR_FTLVL_1));
     // Ожидание окончания передачи
     while (SPI1->SR & SPI_SR_BSY);
     
-    // NSS high
-    io_nss_set(true);
+    // Отключить SPI
+    spi_disable();
+}
+
+void spi_transmit_color(const uint16_t color, uint32_t size)
+{
+    // Включить SPI
+    spi_enable(SPI_DS_16_BIT);
+    
+    for (uint32_t i = 0; i < size; i++)
+    {
+       // Ожидание освобождения буфера передатчика
+        while (!(SPI1->SR & SPI_SR_TXE));
+        // Запись в регистр данных
+        SPI1->DR = color; 
+    }
+    
+    // Ожидание освобождения TxFIFO
+    //while (!(SPI_SR_FTLVL_0 | SPI_SR_FTLVL_1));
+    // Ожидание окончания передачи
+    while (SPI1->SR & SPI_SR_BSY);
     
     // Отключить SPI
     spi_disable();
