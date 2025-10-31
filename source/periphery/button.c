e43d5ee480813c08b51012b2555843665dfcfa55
Павел <130990249+Paul-Maple@users.noreply.github.com>
2025-10-31 17:13:21 +0300

    Переписал порядок включения прерываний EXTI

--- source/periphery/button.c
+++ source/periphery/button.c
@@ -8,23 +8,23 @@
 // TODO: Подобрать правильное время выжидания
 #define BUTTON_CONTACT_BOUNCE_TIME      TIMER_TICKS_MS(10)
 
-// Статическая инициализация кнопки         
-#define BUTTON_STATIC_INIT(mode, timer_cb)                                      \
+// Статическая инициализация кнопки
+#define BUTTON_STATIC_INIT(mode, timer_cb, _name)                               \
 {                                                                               \
     TIMER_STATIC_INIT(mode, timer_cb),                                          \
-    .pressed = false                                                            \
+    .pressed = false,                                                           \
+    .name = _name                                                               \
 }
 
-static void button_switch_state_notice(button_t *button, bool state)
+// Функция оповещения модуля "tetris"
+extern void tetris_button_pressed_notice(button_name_t name, bool state);
+
+static void button_switch_state_notice(button_name_t name, bool state)
 {
-    ASSERT_NULL_PTR(button);
     // Оповещение модулей
-    // module_1_button_switch_state_notice(button, state);
-    // module_2_button_switch_state_notice(button, state);
-    // и т.д.
+    tetris_button_pressed_notice(name, state);
 }
 
-extern void test_3(void);
 // Обработчик события нажатия кнопки
 /* Вызываются как callback у таймера в случае, *
  * если таймер дребезга контактов отработал    */
@@ -38,32 +38,39 @@ static void button_pressed_event_cb(timer_t *timer)
     // Если кнопка была не в нажатом состоянии
     if (!button->pressed)
     {
+        // --- Кнока НАЖАТА --- //
         // Установка флага нажатия кнопки
         button->pressed = true; 
         // Оповещение модулей о нажатии кнопки
-        button_switch_state_notice(button, button->pressed);
-        return;
+        button_switch_state_notice(button->name, button->pressed);
     }
     
     // Кнопка была в нажатом состоянии
     else
     {
+        // --- Кнока ОТПУЩЕНА --- //
         // Установка флага: кнопка не нажата
         button->pressed = false;
         // Оповещение модулей об отпускании кнопки
-        button_switch_state_notice(button, button->pressed);
+        button_switch_state_notice(button->name, button->pressed);
     }
 }
 
 // Кнопки
-static button_t button_0 = BUTTON_STATIC_INIT(TIMER_MODE_ONE_SHOT, button_pressed_event_cb);
-//static button_t button_1 = BUTTON_STATIC_INIT(TIMER_MODE_ONE_SHOT, button_pressed_event_cb);
-//static button_t button_2 = BUTTON_STATIC_INIT(TIMER_MODE_ONE_SHOT, button_pressed_event_cb);
-//static button_t button_3 = BUTTON_STATIC_INIT(TIMER_MODE_ONE_SHOT, button_pressed_event_cb);
+static button_t button_0 = BUTTON_STATIC_INIT(TIMER_MODE_ONE_SHOT, button_pressed_event_cb, BUTTON_NAME_DOWN);
+static button_t button_1 = BUTTON_STATIC_INIT(TIMER_MODE_ONE_SHOT, button_pressed_event_cb, BUTTON_NAME_RIGHT);
+static button_t button_2 = BUTTON_STATIC_INIT(TIMER_MODE_ONE_SHOT, button_pressed_event_cb, BUTTON_NAME_LEFT);
+static button_t button_3 = BUTTON_STATIC_INIT(TIMER_MODE_ONE_SHOT, button_pressed_event_cb, BUTTON_NAME_ROTATE);
 
 void button_init(void)
 {
         /*** Настройка прерываний кнопок ***/
+    // Прерывания на пинах 0, 1, 2, 3 порта С
+    SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PC | 
+                         SYSCFG_EXTICR1_EXTI1_PC | 
+                         SYSCFG_EXTICR1_EXTI2_PC | 
+                         SYSCFG_EXTICR1_EXTI3_PC;
+    
     // Запрос на прерывание с линий EXTI 0, 1, 2, 4
     EXTI->IMR1 |= EXTI_IMR1_IM0 | EXTI_IMR1_IM1 | EXTI_IMR1_IM2 | EXTI_IMR1_IM3;
     
@@ -71,12 +78,12 @@ void button_init(void)
     EXTI->RTSR1 |= EXTI_RTSR1_RT0 | EXTI_RTSR1_RT1 | EXTI_RTSR1_RT2 | EXTI_RTSR1_RT3;
     // Разрешить прерывания по спаду сигнала 
     EXTI->FTSR1 |= EXTI_FTSR1_FT0 | EXTI_FTSR1_FT1 | EXTI_FTSR1_FT2 | EXTI_FTSR1_FT3;
-    
-    // Прерывания на пинах 0, 1, 2, 3 порта С
-    SYSCFG->EXTICR[0] = (SYSCFG_EXTICR1_EXTI0 & SYSCFG_EXTICR1_EXTI0_PC) | 
-                        (SYSCFG_EXTICR1_EXTI1 & SYSCFG_EXTICR1_EXTI1_PC) | 
-                        (SYSCFG_EXTICR1_EXTI2 & SYSCFG_EXTICR1_EXTI2_PC) | 
-                        (SYSCFG_EXTICR1_EXTI3 & SYSCFG_EXTICR1_EXTI3_PC);
+
+    // Сбросить флаги прерывания
+    BUTTON_ISR_FLAG_CLEAR(0);
+    BUTTON_ISR_FLAG_CLEAR(1);
+    BUTTON_ISR_FLAG_CLEAR(2);
+    BUTTON_ISR_FLAG_CLEAR(3);
     
     // Включить перывания в NVIC
     nvic_irq_enable(EXTI0_IRQn);
@@ -101,7 +108,7 @@ void button_1_iqr(void)
     BUTTON_ISR_FLAG_CLEAR(1);
         
     // Запуск таймера для выжидания дребезга контактов
-    timer_start(&button_0.timer, BUTTON_CONTACT_BOUNCE_TIME);
+    timer_start(&button_1.timer, BUTTON_CONTACT_BOUNCE_TIME);
 }
 
 void button_2_iqr(void)
@@ -110,7 +117,7 @@ void button_2_iqr(void)
     BUTTON_ISR_FLAG_CLEAR(2);
         
     // Запуск таймера для выжидания дребезга контактов
-    timer_start(&button_0.timer, BUTTON_CONTACT_BOUNCE_TIME);
+    timer_start(&button_2.timer, BUTTON_CONTACT_BOUNCE_TIME);
 }
 
 void button_3_iqr(void)
@@ -119,5 +126,5 @@ void button_3_iqr(void)
     BUTTON_ISR_FLAG_CLEAR(3);
         
     // Запуск таймера для выжидания дребезга контактов
-    timer_start(&button_0.timer, BUTTON_CONTACT_BOUNCE_TIME);
+    timer_start(&button_3.timer, BUTTON_CONTACT_BOUNCE_TIME);
 }
