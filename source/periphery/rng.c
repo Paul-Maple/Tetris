ded8edbdc10b7043194b51cd5aff83f8320e6eed
Павел <130990249+Paul-Maple@users.noreply.github.com>
2025-10-31 17:11:54 +0300

    Добавил реализацию генерации случайных числе

---
+++ source/periphery/rng.c
@@ -0,0 +1,15 @@
+#include "rng.h"
+
+void rng_init(void)
+{
+    // Включить генератор случайных чисел
+    RNG->CR = RNG_CR_RNGEN;
+}
+
+uint8_t rng_get_number(uint8_t max_number)
+{
+    // Ждём пока регистр не будет доступен для чтения
+    while (!(RNG->SR & RNG_SR_DRDY));
+        // Получить число в диапазоне от 0 до max_number
+         return (RNG->DR & 0xFF) % (max_number + 1);
+}
