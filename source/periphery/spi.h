42a157592809d0ef1eed7e0cafb2b96ae0af3586
Павел <130990249+Paul-Maple@users.noreply.github.com>
2025-11-07 17:16:34 +0300

    Добавлена функция чтения 16 бит

--- source/periphery/spi.h
+++ source/periphery/spi.h
@@ -9,4 +9,7 @@ void spi_transmit(const uint8_t data);
 // Передача 16-битного цвета
 void spi_transmit_color(const uint16_t color, const uint32_t size);
 
+// Получение 16 бит данных 
+void spi_receive(uint16_t *data);
+
 #endif // __SPI_H
