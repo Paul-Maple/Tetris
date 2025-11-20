5eb11807d1e3f0f092a5cc1310a49e4a634c71f1
Павел <130990249+Paul-Maple@users.noreply.github.com>
2025-11-20 12:09:30 +0300

    Перенёл настройку PLL  в модуль clk

--- source/clk.c
+++ source/clk.c
@@ -1,32 +1,58 @@
 #include "clk.h"
 #include "nvic.h"
 #include "event.h"
-
 // Модули, в которых есть тактирование от LSE/LSI
 #include "timer.h"
 
 // События сбоя и стабилизации LSE
-event_t clk_lse_fail_event;
-event_t clk_lse_ready_event;
+static event_t clk_lse_fail_event;
+static event_t clk_lse_ready_event;
 
-// Инициализация источников тактирования (LSI и LSE)
 void clk_init(void)
 {
+        /* Настройка PLL */
+    // Выкл. PLL
+    RCC->CR &= ~RCC_CR_PLLON;
+    while (RCC->CR & RCC_CR_PLLRDY);
+    
+    // Установить задержку на чтение Flash (f <= 80 MHz) и предварительную выборку
+    FLASH->ACR |= FLASH_ACR_LATENCY_4WS | FLASH_ACR_PRFTEN;
+    
+    // Сбросить регистр PLLCFGR
+    RCC->PLLCFGR = 0x0;
+    
+    // f(PLLCLK) = (( 4 MHz / 1 ) * 80 ) / 4 = 80 MHz                           // PLLM = 1
+    RCC->PLLCFGR =  RCC_PLLCFGR_PLLN_4 | RCC_PLLCFGR_PLLN_6 |                   // PLLN = 80
+                    RCC_PLLCFGR_PLLR_0;                                         // PLLR = 4
+    
+    // Вход PLL - MSI
+    RCC->PLLCFGR |= RCC_PLLCFGR_PLLSRC_MSI;
+    
+    // Вкл. PLL
+    RCC->CR |= RCC_CR_PLLON;
+    // Ожидание готовности PLL
+    while (!(RCC->CR & RCC_CR_PLLRDY));
+    // Вкл. выход PLLCLK
+    RCC->PLLCFGR |= RCC_PLLCFGR_PLLREN;
+    // PLL selected as system clock
+    RCC->CFGR |= RCC_CFGR_SW_PLL;
+    //  Ожидание переключения системной частоты на PLL
+    while (!(RCC->CFGR & RCC_CFGR_SWS_PLL));
+    
     // Инициализация событий стабилизации/сбоя LSE
     event_init(&clk_lse_ready_event, clk_lse_ready);
     event_init(&clk_lse_fail_event, clk_lse_fail);
-
-        /* Запуск LSI */
-    // Разрешить работу LSI
-    RCC->CSR = RCC_CSR_LSION;
     
-        /* Запуск LSE */  
-    PWR->CR1 |= PWR_CR1_DBP;                            // Разрешаем запись в регистр RCC_BDCR
-    RCC->BDCR = RCC_BDCR_BDRST;                         // Сбросить все значения в регисте BDCR
-    RCC->CIER = RCC_CIER_LSERDYIE | RCC_CIER_LSECSSIE;  // Разрешить прерывание, вызванное стабилизацией и сбоем LSE
-    EXTI->IMR1 = EXTI_IMR1_IM19;                        // Включить запрос прерывания с LSE 
-    EXTI->RTSR1 = EXTI_RTSR1_RT19;                      // Триггер по фронту сигнала прерывания
-    RCC->BDCR = RCC_BDCR_LSEON;                         // Разрешить работу LSE
+        /* Запуск LSI */
+    RCC->CSR = RCC_CSR_LSION;           // Разрешить работу LSI
+    
+        /* Запуск LSE */
+    PWR->CR1 |= PWR_CR1_DBP;            // Разрешаем запись в регистр RCC_BDCR
+    RCC->BDCR = RCC_BDCR_BDRST;         // Сбросить все значения в регисте BDCR
+    RCC->CIER = RCC_CIER_LSERDYIE;      // Разрешить прерывание, вызванное стабилизацией LSE
+    EXTI->IMR1 = EXTI_IMR1_IM19;        // Включить запрос прерывания с LSE 
+    EXTI->RTSR1 = EXTI_RTSR1_RT19;      // Триггер по фронту сигнала прерывания
+    RCC->BDCR = RCC_BDCR_LSEON;         // Разрешить работу LSE
     
     // Включить прерывания LSE в NVIC
     nvic_irq_enable(TAMP_STAMP_IRQn);   // Сбой
@@ -53,12 +79,11 @@ void clk_lse_fail(void)
 {
     // Сбросить все значения в регисте BDCR
     RCC->BDCR = RCC_BDCR_BDRST;
-    // Разрешить работу LSE
+    // Запуск LSE после сбоя
     RCC->BDCR = RCC_BDCR_LSEON;
     // Оповещение модуля led о смене источника тактирования
     clk_lf_src_changed(CLK_LF_SRC_LSI);    
 }
-
 // Обработчик готовности LSE
 void clk_lse_ready(void)
 {
@@ -75,7 +100,7 @@ void clk_lse_ready(void)
 void clk_lse_fail_isr(void)
 {   
     // Сброс флагов прерывания сбоя LSE 
-    RCC->CICR = RCC_CICR_LSECSSC;
+    RCC->CICR |= RCC_CICR_LSECSSC;
     EXTI->PR1 |= EXTI_PR1_PIF19;
     
     // Запретить прерывание, вызванное сбоем LSE
--- source/mcu.c
+++ source/mcu.c
@@ -6,7 +6,7 @@ void mcu_init(void)
         /* Тактирование */
     // Включить тактирование таймера и контроля питания (Для доступа к записи в регистр RCC_BDCR)
     // RCC_BDCR защищен от записи и требует предварительно разблокировки
-    RCC->APB1ENR1 = RCC_APB1ENR1_PWREN | RCC_APB1ENR1_LPTIM1EN;
+    RCC->APB1ENR1 = RCC_APB1ENR1_LPTIM1EN | RCC_APB1ENR1_PWREN;
     
     // Тактирование регистра SYSCFG для настройки прерываний EXTI
     RCC->APB2ENR = RCC_APB2ENR_SYSCFGEN;
@@ -15,35 +15,7 @@ void mcu_init(void)
     RCC->AHB2ENR = RCC_AHB2ENR_GPIOAEN | RCC_AHB2ENR_GPIOBEN |                  // Порты А, В
                    RCC_AHB2ENR_GPIOCEN | RCC_AHB2ENR_GPIOHEN;                   // Порты С, Н
     
-    // Включение MCO для вывода частоты на GPIO для проверки
-    RCC->CFGR = //RCC_CFGR_MCOPRE_1 |                     // MCO = CLK / 4  (Раскомментирую при более высоких частотах)
-                //RCC_CFGR_MCOSEL_0 | RCC_CFGR_MCOSEL_2;  // PLL
-                RCC_CFGR_MCOSEL_0;                      // SYSCLK
-    
-        /* Настройка PLL */
-    // Выкл. PLL
-    /*RCC->CR &= ~RCC_CR_PLLON;
-    while (RCC->CR & RCC_CR_PLLRDY);
-    
-    // Вход PLL - MSI
-    RCC->PLLCFGR |= RCC_PLLCFGR_PLLSRC_MSI;
-    
-    // f(VCO clock) = f(PLL clock input) * (PLLN / PLLM) = 4 MHz * ( 40 / 1 ) = 160 MHz
-    // f (PLLCLK) = VCO / PLLR = 160 MHz / 4 = 40 MHz
-    RCC->PLLCFGR &= ~(RCC_PLLCFGR_PLLM_0 | RCC_PLLCFGR_PLLM_1 | RCC_PLLCFGR_PLLM_2);    // PLLM = 1
-    RCC->PLLCFGR |= //RCC_PLLCFGR_PLLN_3 | RCC_PLLCFGR_PLLN_5 |                           // PLLN = 40
-                    RCC_PLLCFGR_PLLN_2 | RCC_PLLCFGR_PLLN_4 |                           // PLLN = 20 - при 20 всё норм
-                    RCC_PLLCFGR_PLLR_0;                                                 // PLLR = 4
-    
-    // Вкл. PLL
-    RCC->CR |= RCC_CR_PLLON;
-    // Вкл. выход PLLCLK
-    RCC->PLLCFGR |= RCC_PLLCFGR_PLLREN;
-    // Ожидание готовности PLL
-    while (!(RCC->CR & RCC_CR_PLLRDY));
-    // Установить задержку на чтение Flash memory (f <= 48 MHz)
-    FLASH->ACR |= FLASH_ACR_LATENCY_2WS;
-    // PLL selected as system clock
-    RCC->CFGR |= RCC_CFGR_SW_PLL;
-*/
+    // Включение MCO для вывода системной частоты на GPIO
+    RCC->CFGR |= RCC_CFGR_MCOPRE_1 |    // MCO = CLK / 4
+                 RCC_CFGR_MCOSEL_0;     // SYSCLK
 }
